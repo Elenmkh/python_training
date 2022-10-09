277d62ce9573cdf89cda5d6c07ff2d4db8b02a33
Elena Makhnach <e.makhnach@mail.ru>
2022-10-09 20:34:02 +0300

    Исправлен записаный тест

--- test_add_group.py
+++ test_add_group.py
@@ -7,13 +7,13 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class TestAddGroup(unittest.TestCase):
+
+class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome(executable_path=r'')
+        self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    
-    def test_add_group(self):
+    def test_untitled_test_case(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("user").click()
@@ -27,30 +27,31 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("gggf")
+        wd.find_element_by_name("group_name").send_keys("ggg")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("dfdfd")
+        wd.find_element_by_name("group_header").send_keys("ffff")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("fdfdfd")
+        wd.find_element_by_name("group_footer").send_keys("hghg")
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
-        #ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
         wd.find_element_by_link_text("Logout").click()
-        #ERROR: Caught exception [unknown command []]
-    
+
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
 
-    
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
     def tearDown(self):
         self.wd.quit()
 
