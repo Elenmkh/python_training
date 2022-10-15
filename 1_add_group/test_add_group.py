83bab8d936239aead9fe2e7709ab1a51e47f4b1c
Elena Makhnach <e.makhnach@mail.ru>
2022-10-15 21:37:44 +0300

    reset

---
+++ 1_add_group/test_add_group.py
@@ -0,0 +1,21 @@
+import pytest
+from group import Group
+from application_group import Application
+
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="ffff", header="ggg", footer="hghg"))
+    app.logout()
+
+
+def test_test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
