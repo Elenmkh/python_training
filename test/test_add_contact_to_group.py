da0e4590e6c4a90f87e666b4a82f9e0e96b6d48d
Elena Makhnach <e.makhnach@mail.ru>
2022-12-05 21:18:32 +0300

    Исправление в тесте на добавление контакта в группу

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -8,15 +8,16 @@ import random
 def test_add_contact_to_group(app, json_contact, orm_db):
     #db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
     groups = orm_db.get_group_list()
-    contacts = orm_db.get_contact_list()
     if len(groups) == 0:
         app.group.create(Group(name="test"))
         groups = orm_db.get_group_list()
+    random_group = random.choice(groups)
+    contacts = orm_db.get_contacts_not_in_group(random_group)
     if len(contacts) == 0:
         app.contact.add()
         app.contact.fill_contact_form(json_contact)
         app.contact.aply_create()
-    random_group = random.choice(groups)
+        contacts = orm_db.get_contacts_not_in_group(random_group)
     random_contact = random.choice(contacts)
     old_contacts = orm_db.get_contacts_in_group(random_group)
     app.contact.add_contact_to_group_by_id(random_contact.id, random_group.id)
